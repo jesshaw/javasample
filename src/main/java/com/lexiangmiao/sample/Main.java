1734d522b0cbc138c3b7f81ab029bfa2bed90920
mzxiao <mzxiao@ctrip.com>
2020-07-28 16:01:02 +0800

    spring boot mysql unit test

---
+++ src/main/java/com/lexiangmiao/sample/Main.java
@@ -0,0 +1,14 @@
+package com.lexiangmiao.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by mzxiao on 2020/7/28 15:46
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
