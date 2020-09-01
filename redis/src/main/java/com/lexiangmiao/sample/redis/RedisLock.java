40b9475cabff7d1fa6348a1d2663102532d71579
jesshaw <jesshaw@126.com>
2020-09-01 16:25:49 +0800

    distributed lock v8  renew the time

--- redis/src/main/java/com/lexiangmiao/sample/redis/RedisLock.java
+++ redis/src/main/java/com/lexiangmiao/sample/redis/RedisLock.java
@@ -4,10 +4,12 @@ import java.util.concurrent.TimeUnit;
 
 /**
  *
+ *         AP 高可用
  *         互斥性
  *         超时 解决断电或kill
  *         支持阻塞非阻塞
  *         可重入   不能多次 setnx
+ *         高可用
  *
  */
 public interface RedisLock {
--- redis/src/main/java/com/lexiangmiao/sample/redis/RedisLockImpl.java
+++ redis/src/main/java/com/lexiangmiao/sample/redis/RedisLockImpl.java
@@ -9,16 +9,49 @@ import java.util.concurrent.TimeUnit;
 
 @Component
 public class RedisLockImpl implements RedisLock {
+    public static final String split = ":";
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
-
     private ThreadLocal<String> threadLocal = new ThreadLocal<>();
+
+    public static Thread findThread(long threadId) {
+        ThreadGroup group = Thread.currentThread().getThreadGroup();
+        while (group != null) {
+            Thread[] threads = new Thread[(int) (group.activeCount() * 1.2)];
+            int count = group.enumerate(threads, true);
+            for (int i = 0; i < count; i++) {
+                if (threadId == threads[i].getId()) {
+                    return threads[i];
+                }
+            }
+            group = group.getParent();
+        }
+        return null;
+    }
 
     @Override
     public boolean tryLock(String key, long timeout, TimeUnit unit) {
         boolean lock = false;
         if (threadLocal.get() == null) {
-            String uuid = UUID.randomUUID().toString();
+
+            // 异步续命
+            Thread thread = new Thread() {
+                @Override
+                public void run() {
+                    while (!Thread.interrupted()) {
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println("renew the time ...");
+                        stringRedisTemplate.expire(key, timeout, unit);
+                    }
+                    System.out.println("renew stop ...");
+                }
+            };
+
+            String uuid = thread.getId() + split + UUID.randomUUID().toString();
             threadLocal.set(uuid);
             lock = stringRedisTemplate.opsForValue().setIfAbsent(key, uuid, timeout, unit);
             while (!lock) { //自旋锁
@@ -27,6 +60,10 @@ public class RedisLockImpl implements RedisLock {
                     break;
                 }
             }
+
+            thread.start(); //拿到锁的线程才会启动续命线程
+
+
         } else if (threadLocal.get().equals(stringRedisTemplate.opsForValue().get(key))) {
             lock = true;
         }
@@ -37,6 +74,10 @@ public class RedisLockImpl implements RedisLock {
     @Override
     public void releaseLock(String key) {
         if (threadLocal.get().equals(stringRedisTemplate.opsForValue().get(key))) {
+            Thread thread = findThread(Long.parseLong(threadLocal.get().split(split)[0]));
+            if (thread != null) {
+                thread.interrupt();
+            }
             stringRedisTemplate.delete(key);
         }
     }
