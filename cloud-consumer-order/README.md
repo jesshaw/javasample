920818ba3bcb01618549eaf2b851a5b5a8327fa1
jesshaw <jesshaw@126.com>
2020-09-09 09:45:52 +0800

    customer ribbon rule

--- cloud-consumer-order/README.md
+++ cloud-consumer-order/README.md
@@ -1,3 +1,5 @@
 # 知识点
 1. resetTemplate
 1. 负载均衡
+1. 自定义负载均衡 
+   1. 不能放在ComponentScan扫描的包以及子包子
--- cloud-consumer-order/src/main/java/com/lexiangmiao/sample/springcloud/OrderApp.java
+++ cloud-consumer-order/src/main/java/com/lexiangmiao/sample/springcloud/OrderApp.java
@@ -1,12 +1,15 @@
 package com.lexiangmiao.sample.springcloud;
 
+import com.lexiangmiao.sample.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 public class OrderApp {
     public static void main(String[] args) {
         SpringApplication.run(OrderApp.class, args);
