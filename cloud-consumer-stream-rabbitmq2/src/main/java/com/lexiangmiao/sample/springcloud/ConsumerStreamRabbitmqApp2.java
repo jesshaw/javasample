c6834b9c85e63f27d13de18be6f376ca6b88f572
jesshaw <jesshaw@126.com>
2020-09-11 10:30:33 +0800

    Avoid repeated consumption

---
+++ cloud-consumer-stream-rabbitmq2/src/main/java/com/lexiangmiao/sample/springcloud/ConsumerStreamRabbitmqApp2.java
@@ -0,0 +1,11 @@
+package com.lexiangmiao.sample.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerStreamRabbitmqApp2 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerStreamRabbitmqApp2.class, args);
+    }
+}
---
+++ cloud-consumer-stream-rabbitmq2/src/main/java/com/lexiangmiao/sample/springcloud/controller/ReceiveMessageController.java
@@ -0,0 +1,22 @@
+package com.lexiangmiao.sample.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableBinding(Sink.class)
+@Slf4j
+public class ReceiveMessageController {
+    @Value("${server.port}")
+    String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void receive(Message<String> message) {
+        log.info("********消费者2号接收到的消息：" + message.getPayload() + "\t port: " + serverPort);
+    }
+}
